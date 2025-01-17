14c1a9c26abb062571e1dd89acacc46f26408876
Matt Raynor <mattraynor@Matts-MacBook-Pro.local>
2025-01-17 16:22:59 -0500

    tests i dont understand

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+# pages/tests.py
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve # new
+from .views import HomePageView # ne
+class HomepageTests(SimpleTestCase): 
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
 
-# Create your tests here.
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page.'
+        )
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
+
+
